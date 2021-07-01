ad3959903d2d21c014f6a1cd5cae75966ba143c9
NatachaVergara <ntchvergara@gmail.com>
2021-07-01 18:53:43 -0300

    Agregue la seccion Contacto

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const rutas = express.Router();
 
+
+let hora = new Date();
+rutas.use(function timeLog (req, res, next) {
+    console.log('Time', hora.toLocaleString())
+    next()
+  })
+
 rutas.get('/', (req, res) =>{
-        res.render('index.html', {title: 'Primer Website'});
+        res.render('index.html', {title: 'Inicio: Bienvenidos'});
 })
 
 rutas.get('/restaurantes', (req, res) =>{
     res.render('restaurantes.html', {title: 'Restaurantes'});
+})
+
+rutas.get('/contacto', (req, res) =>{
+    res.render('contacto.html', {title: 'Contacto'});
 })
 
 
